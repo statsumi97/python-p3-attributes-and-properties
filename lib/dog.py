72b2e6eaf1eb2b295011dae10540573435c1441f
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2023-12-15 11:20:18 -0500

    final product

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,15 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name=None, breed=None):
+        if name is not None:
+            if not isinstance(name, str) or not (1 <= len(name) <= 25):
+                print('Name must be string between 1 and 25 characters.')
+            else:
+                self.name = name
+
+        if breed is not None:
+            if breed not in APPROVED_BREEDS:
+                print('Breed must be in list of approved breeds.')
+            else:
+                self.breed = breed
